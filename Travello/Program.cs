b7ac3fc6da9bab1cf996e60fc1c2eec68ad707ed
mohamedhw <mohamedhw2000@gmail.com>
2025-06-02 00:58:30 +0300

    registered the unitOfWork

--- Travello/Program.cs
+++ Travello/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
