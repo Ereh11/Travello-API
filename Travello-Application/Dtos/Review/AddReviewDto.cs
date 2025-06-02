c91d741dfed420ba4e7678fdc0bdf1a17d5a5c7b
‪Hany Abdou Saad‬‏ <Hanysaadstd@gmail.com>
2025-06-03 02:24:11 +0300

    Update binaries and assembly information for Travello project
    - Updated Travello.dll, Travello.exe, and Travello.pdb binaries.
    - Changed AssemblyInformationalVersion in Travello.AssemblyInfo.cs.
    - Updated cache files for assembly references and compile inputs.
    - Added new dependencies to the project file list.
    - Updated sourcelink.json to reflect the latest commit.

--- Travello-Application/Dtos/Review/AddReviewDto.cs
+++ Travello-Application/Dtos/Review/AddReviewDto.cs
@@ -1,13 +1,12 @@
 ﻿using Travello_Domain;
 
+
 namespace Travello_Application.Dtos.Review
 {
     public class AddReviewDto
     {
         public Guid UserId { get; set; }
-        public User User { get; set; } = null!;
         public Guid HotelId { get; set; }
-        public Hotel Hotel { get; set; } = null!;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public string Comment { get; set; } = string.Empty;
         public int Rating { get; set; }
