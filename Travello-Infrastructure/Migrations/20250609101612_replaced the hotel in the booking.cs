d3a549e2e0a5177bf3bcdb0c99fead680704b1ac
mohamedhw <mohamedhw2000@gmail.com>
2025-06-09 13:21:52 +0300

    changed the db from azure to somee

---
+++ Travello-Infrastructure/Migrations/20250609101612_replaced the hotel in the booking.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Travello_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class replacedthehotelinthebooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Payments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "HotelId",
+                table: "Bookings",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AccommodationId",
+                table: "Bookings",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_AccommodationId",
+                table: "Bookings",
+                column: "AccommodationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Accommodations_AccommodationId",
+                table: "Bookings",
+                column: "AccommodationId",
+                principalTable: "Accommodations",
+                principalColumn: "AccommodationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Accommodations_AccommodationId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_AccommodationId",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "AccommodationId",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "HotelId",
+                table: "Bookings",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
