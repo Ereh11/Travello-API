f37067e8fc20ee279403b9234d61d970d808d8c6
Mostafa <enmostafabr@gmail.com>
2025-06-11 00:18:47 +0300

    add con string

--- Travello/Program.cs
+++ Travello/Program.cs
@@ -31,7 +31,11 @@ if (app.Environment.IsDevelopment())
 // Use Swagger UI in development
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Travello API v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
